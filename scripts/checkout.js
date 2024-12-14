4cef026edf7ab4c5f8784c99fcb51701683bea25
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2024-12-14 22:30:44 +0100

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { deliveryOptions } from "../data/deliveryOptions.js";
@@ -90,7 +90,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html +=`
-          <div class="delivery-option">
+          <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id} data-delivery-option-id = "${deliveryOption.id}>
       <input
         type="radio"
         ${isChecked ?'checked' : ''}
@@ -130,4 +130,11 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
         document.querySelector('.js-qunatity-input').setAttribute('class', 'show');
       }
     })
+  });
+
+  document.querySelectorAll('js-delivery-option').forEach((element) =>{
+    element.addEventListener('click', () =>{
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+    });
   });
