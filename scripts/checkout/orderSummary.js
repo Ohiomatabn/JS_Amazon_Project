824f8bf3a3fe5a048ecd39db09cb11d3e2119812
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2024-12-18 18:24:51 +0100

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,7 +1,7 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import { products } from '../../data/products.js';
+import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import dayjs from 'https://www.unpkg.com/dayjs@1.11.10/esm/index.js';
 
 
@@ -15,23 +15,11 @@ export function renderOderSummary(){
   cart.forEach((cartItem) =>{
     const productId = cartItem.productId;
 
-    let matchingProduct;
-
-  products.forEach((product) =>{
-    if (product.id === productId){
-      matchingProduct = product;
-    }
-  });
-
-  let deliveryOption;
+    const matchingProduct = getProduct(productId)
 
   const deliveryOptionId = cartItem.deliveryOptionId;
 
-  deliveryOptions.forEach((option) =>{
-    if(option.id === deliveryOptionId){
-      deliveryOption = option
-    }
-  });
+  const deliveryOption = getDeliveryOption(deliveryOptionId);
 
   cartSummaryHTML +=
     `
