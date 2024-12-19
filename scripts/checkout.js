6e9872f0ab9b81f74b6c48661f9ce3d142ca5749
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2024-12-19 21:22:56 +0100

    Update the checkout numbers to match the exact numbers of produt

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,21 @@
 import { renderOderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import {cart} from "../data/cart.js";
+
+function updateCheckoutItem(){
+  let items = 0;
+
+  cart.forEach((cartItem) =>{
+    items += cartItem.quantity;
+  });
+  
+  let itemsHTML = document.getElementsByClassName('js-return-to-home-link');
+  itemsHTML.innerHTML = items;
+
+  return items
+}
+
+document.querySelector('.js-return-to-home-link').innerHTML = updateCheckoutItem();
 
 renderOderSummary();
 renderPaymentSummary();
