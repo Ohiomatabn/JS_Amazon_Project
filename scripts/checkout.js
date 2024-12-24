dfa98e9f22e03d6df2b5fde2d7c1090410856bb7
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2024-12-24 15:52:13 +0100

    create oop versionof cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,21 +1,7 @@
 import { renderOderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import {cart} from "../data/cart.js";
+import  "../data/cart-oop.js";
 
-function updateCheckoutItem(){
-  let items = 0;
-
-  cart.forEach((cartItem) =>{
-    items += cartItem.quantity;
-  });
-  
-  let itemsHTML = document.getElementsByClassName('js-return-to-home-link');
-  itemsHTML.innerHTML = items;
-
-  return items
-}
-
-document.querySelector('.js-return-to-home-link').innerHTML += `${updateCheckoutItem()} items`;
 
 renderOderSummary();
 renderPaymentSummary();
