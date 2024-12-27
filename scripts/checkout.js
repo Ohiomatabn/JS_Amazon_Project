b784f21276380ef783153bffac672165033049d1
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2024-12-27 20:22:45 +0100

    Learn promises and convert code to promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,51 @@
 import { renderOderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import {loadProduct} from '../data/products.js';
+import loadCart from '../data/cart.js';
 //import  "../data/cart-class.js";
 //import '../data/backend-practice.js'
 
-loadProduct(() =>{
+Promise.all([
+  new Promise((resolve) =>{
+    loadProduct(() =>{
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) =>{
+    loadCart(() =>{
+      resolve();
+    });
+  })
+
+]).then((values) =>{
+  console.log(values);
   renderOderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) =>{
+  loadProduct(() =>{
+    resolve();
+  });
+
+}).then(() =>{
+  return new Promise((resolve) =>{
+    loadCart(() =>{
+      resolve();
+    });
+  });
+
+}).then(() =>{
+  renderOderSummary();
+  renderPaymentSummary();
+});
+*/
+
+
+/*loadProduct(() =>{
+  loadCart(() =>{
+    renderOderSummary();
+    renderPaymentSummary();
+  });
+});*/
